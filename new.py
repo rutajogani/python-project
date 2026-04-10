18abc0abdea60fd9b0e3efcf2cb19bc593f058fa
Ruta Jogani <ruta007jogani@gmail.com>
2026-04-10 23:58:57 +0530

    add uv  file with python file and add some line in python file

--- new.py
+++ new.py
@@ -1,5 +1,14 @@
 import pyttsx3
+import speech_recognition as sr
+
 engine = pyttsx3.init()
 engine.say('Sally sells seashells by the seashore.')
 engine.say('The quick brown fox jumped over the lazy dog.')
 engine.runAndWait()
+
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    audio = r.listen(source)
+
+print(r.recognize_google(audio))
