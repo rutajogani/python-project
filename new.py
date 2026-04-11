1c3c5821f676ac77d0cafd7d4752b92fddd93296
Ruta Jogani <ruta007jogani@gmail.com>
2026-04-11 23:05:27 +0530

    remove some line of code

--- new.py
+++ new.py
@@ -5,10 +5,3 @@ engine = pyttsx3.init()
 engine.say('Sally sells seashells by the seashore.')
 engine.say('The quick brown fox jumped over the lazy dog.')
 engine.runAndWait()
-
-
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    audio = r.listen(source)
-
-print(r.recognize_google(audio))
