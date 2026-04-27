7372b3f080646ef181c63fc1973c4d95353148ac
Ruta Jogani <ruta007jogani@gmail.com>
2026-04-27 23:10:11 +0530

    add : line of code and add also uv file

--- new.py
+++ new.py
@@ -1,7 +1,8 @@
-import pyttsx3
 import speech_recognition as sr
 
-engine = pyttsx3.init()
-engine.say('Sally sells seashells by the seashore.')
-engine.say('The quick brown fox jumped over the lazy dog.')
-engine.runAndWait()
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    audio = r.listen(source)
+
+text = r.recognize_google(audio)
+print(text)
