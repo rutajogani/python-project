6932c988d9da540666188aaeca8ded5cd0e32642
Ruta Jogani <ruta007jogani@gmail.com>
2026-04-29 15:14:33 +0530

    add stt and tts function

--- new.py
+++ new.py
@@ -1,8 +1,14 @@
 import speech_recognition as sr
+import pyttsx3
 
+#stt statement
 r = sr.Recognizer()
 with sr.Microphone() as source:
     audio = r.listen(source)
-
 text = r.recognize_google(audio)
 print(text)
+
+# tts statement
+engine = pyttsx3.init()
+engine.say(text)
+engine.runAndWait()
