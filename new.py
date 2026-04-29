a49d37808ecefb17ddc8f0335847800c8211746d
Ruta Jogani <ruta007jogani@gmail.com>
2026-04-29 22:17:40 +0530

    add: code add tts and stt statement

--- new.py
+++ new.py
@@ -6,9 +6,16 @@ r = sr.Recognizer()
 with sr.Microphone() as source:
     audio = r.listen(source)
 text = r.recognize_google(audio)
-print(text)
+print("USER SPOKE: " + text)
 
 # tts statement
 engine = pyttsx3.init()
-engine.say(text)
+engine.setProperty('rate', 120) 
+
+things = ["phone", "TV", "bag"]
+for thing in things:
+    if thing in text:
+        print("FOUND IT")
+        engine.say("this is your thing")
+
 engine.runAndWait()
