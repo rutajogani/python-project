7ce2b17d736a09740ac82a460d7cd6a512a6afd2
Ruta Jogani <ruta007jogani@gmail.com>
2026-04-30 18:24:24 +0530

    add uv file and also add some lines of code in new.py file

--- new.py
+++ new.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
 import pyttsx3
+from datetime import datetime
 
 #stt statement
 r = sr.Recognizer()
@@ -12,10 +13,38 @@ print("USER SPOKE: " + text)
 engine = pyttsx3.init()
 engine.setProperty('rate', 120) 
 
-things = ["phone", "TV", "bag"]
+def date_time():
+    #date and time function
+    now_date = datetime.now()
+    date_now = now_date.strftime("%d-%m-%Y")
+    now_time = datetime.now()
+    time_now = now_time.strftime("%H:%M:%S")
+
+    date_time = ["date","time"]
+
+    if "date" in text:
+        print(date_now)
+        engine.say("date is here:")
+        print("----Here is your Date----")
+    else:
+        print(time_now)
+        engine.say("time is here:")
+        print("----Here is your Time----")
+
+things = {
+    "phone": "Here is your phone",
+    "tv": "Turning on the TV",
+    "bag": "Here is your bag"
+}
+
 for thing in things:
     if thing in text:
         print("FOUND IT")
-        engine.say("this is your thing")
+        engine.say(things[thing])
+    
+else:
+        print("Not Found")
 
 engine.runAndWait()
+
+date_time()
