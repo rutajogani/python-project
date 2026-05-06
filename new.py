97c7164dd409a6f8607655d32a7fbaa3ea003025
Ruta Jogani <ruta007jogani@gmail.com>
2026-05-06 19:37:05 +0530

    add : some code in new.py file

--- new.py
+++ new.py
@@ -46,10 +46,10 @@ def web_browser():
                     what_to_search = search_recognizer.recognize_google(audio)
                     searching = f"http://www.google.com/search?q={what_to_search}"
 
-                    web.open("https://www.google.com/")
-                    print("----Google opened----")
-                    engine.say("Google opened")
-                    break
+            web.open("https://www.google.com/")
+            print("----Google opened----")
+            engine.say("Google opened")
+            break
 
         elif "monkey type" in text:
             web.open("https://monkeytype.com/")
