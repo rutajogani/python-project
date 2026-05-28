a63f782805e0616ac69c14081390e8e65051ae2d
Ruta Jogani <ruta007jogani@gmail.com>
2026-05-28 23:29:03 +0530

    remove some line of code in main.py file

--- main.py
+++ main.py
@@ -8,21 +8,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-# Selenium
-driver = webdriver.Chrome()
-driver.quit()
 
-if "youtube" in text.lower():
-    driver.get("https://youtube.com")
-    engine.say("Opening YouTube")
-    engine.runAndWait()
+text = listen()
 
-# engine = pyttsx3.init()
-# engine.setProperty('rate', 120) 
-# text = listen()
-# speak(text)
+speak(text)
 
-# password()
-# date_time(text)
-# web_browser(text)
-# things(text)
+password()
+date_time(text)
+web_browser(text)
+things(text)
